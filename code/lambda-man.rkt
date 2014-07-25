fb6c6e8ccd36144fb04432af98a6722cd106d63d
Phil Nguyen <pcn@cs.umd.edu>
2014-07-25 14:09:58 -0400

    format

--- code/lambda-man.rkt
+++ code/lambda-man.rkt
@@ -3,35 +3,35 @@
 
 (define-language L
   ;; GCC syntax
-  [gcc (LDC $n)
-       (LD $n $i)
-       ADD SUB MUL DIV
-       CEQ CGT CGTE
-       ATOM
-       CONS CAR CDR
-       (SEL $t $f)
-       JOIN
-       (LDF $f)
-       (AP $n)
-       RTN
-       (DUM $n)
-       (RAP $n)
-       STOP
-       ; Tail call extensions
-       (TSEL $t $f)
-       (TAP $n)
-       (TRAP $n)
-       ; Pascal extensions
-       (ST $n $i)
-       ; Debug extensions
-       DBUG BRK]
+  [(gcc gcc₀ gcc₁ gcc₂) (LDC $n)
+                        (LD $n $i)
+                        ADD SUB MUL DIV
+                        CEQ CGT CGTE
+                        ATOM
+                        CONS CAR CDR
+                        (SEL $t $f)
+                        JOIN
+                        (LDF $f)
+                        (AP $n)
+                        RTN
+                        (DUM $n)
+                        (RAP $n)
+                        STOP
+                        ; Tail call extensions
+                        (TSEL $t $f)
+                        (TAP $n)
+                        (TRAP $n)
+                        ; Pascal extensions
+                        (ST $n $i)
+                        ; Debug extensions
+                        DBUG BRK]
   [GCC (gcc ... dec ...)]
-  [dec (: ℓ gcc ...)]
+  [(dec dec₀ dec₁ dec₂) (: ℓ gcc ...)]
   [($n $i $t $f) integer ℓ #|FIXME|#]
   [(m n i) integer]
-  [(ℓ ℓ₁ ℓ₂) variable-not-otherwise-mentioned]
+  [(ℓ ℓ₀ ℓ₁ ℓ₂) variable-not-otherwise-mentioned]
   ;; basic language
-  [(e e₁ e₂ eₓ) (λ (x ...) e) (e e ...) (o₁ e) (o₂ e e) (if e e e) x b n]
+  [(e e₀ e₁ e₂ eₓ) (λ (x ...) e) (e e ...) (o₁ e) (o₂ e e) (if e e e) x b n]
   [o₁ CAR CDR ATOM]
   [o₂ ADD SUB MUL DIV CEQ CGT CGTE CONS]
   [o o₁ o₂]
@@ -62,8 +62,13 @@
   [(t ρ x) ([LD n i]) (where (n i) (index-of ρ x))]
   [(t ρ (o e ...)) (gcc ... o dec ...)
    (where (gcc ... dec ...) (t* ρ e ...))]
-  [(t ρ (if e ...)) (error "TODO")
-   (where (gcc ... dec ...) (t* ρ e ...))]
+  [(t ρ (if e₀ e₁ e₂))
+   (gcc₀ ... (SEL ℓ₁ ℓ₂) JOIN dec₀ ... dec₁ ... dec₂ ... (: ℓ₁ gcc₁ ...) (: ℓ₂ gcc₂ ...))
+   (where (gcc₀ ... dec₀ ...) (t ρ e₀))
+   (where (gcc₁ ... dec₁ ...) (t ρ e₁))
+   (where (gcc₂ ... dec₂ ...) (t ρ e₂))
+   (where ℓ₁ ,(fresh-label!))
+   (where ℓ₂ ,(fresh-label!))]
   [(t ρ (e eₓ ...)) (gcc_1 ... gcc_i ... [AP n] RTN dec_1 ... dec_i ...)
    (where (gcc_1 ... dec_1 ...) (t ρ e))
    (where (gcc_i ... dec_i ...) (t* ρ eₓ ...))
@@ -92,9 +97,12 @@
          [`(: ,ℓ) (format "~a:" ℓ)]
          [(? list? l) (string-join (for/list ([x l]) (format "~a" x)) " " #:before-first "  ")]
          [x (format "  ~a" x)]))
-     "\n")])
+     "\n"
+     #:after-last "\n")])
 
 ;; Example programs
 (define-term ex1
   ((λ (x) (ADD x x)) 21))
 
+(define-term ex2
+  ((λ (x) (if x 2 3)) 42))
