201b6a3f20f45b6bbf49f42b920749e1a1382ea1
Phil Nguyen <pcn@cs.umd.edu>
2014-07-26 00:00:47 -0400

    convert symbolic address to absolute one

--- code/lambdaman.rkt
+++ code/lambdaman.rkt
@@ -1,5 +1,5 @@
 #lang racket/base
-(require racket/match racket/string redex/reduction-semantics)
+(require racket/match racket/string racket/list redex/reduction-semantics)
 
 (define-language L
   ;; GCC syntax
@@ -24,7 +24,9 @@
                         ; Pascal extensions
                         (ST $n $i)
                         ; Debug extensions
-                        DBUG BRK]
+                        DBUG BRK
+                        ; target language comment
+                        (cmt string)]
   [GCC (gcc ... dec ...)]
   [(dec dec₀ dec₁ dec₂) (: ℓ gcc ...)]
   [($n $i $t $f) integer ℓ #|FIXME|#]
@@ -37,7 +39,9 @@
   [o o₁ o₂]
   [b boolean]
   [ρ ((x ...) ...)]
-  [(x y z) variable-not-otherwise-mentioned])
+  [(x y z) variable-not-otherwise-mentioned]
+  ;;
+  [symtab (side-condition (name symtab any) (hash? (term symtab)))])
 
 ;; syntactic sugar
 (define-metafunction L
@@ -63,7 +67,7 @@
   [(t ρ (o e ...)) (gcc ... o dec ...)
    (where (gcc ... dec ...) (t* ρ e ...))]
   [(t ρ (if e₀ e₁ e₂))
-   (gcc₀ ... (SEL ℓ₁ ℓ₂) JOIN dec₀ ... dec₁ ... dec₂ ... (: ℓ₁ gcc₁ ...) (: ℓ₂ gcc₂ ...))
+   (gcc₀ ... (SEL ℓ₁ ℓ₂) dec₀ ... dec₁ ... dec₂ ... (: ℓ₁ gcc₁ ... JOIN) (: ℓ₂ gcc₂ ... JOIN))
    (where (gcc₀ ... dec₀ ...) (t ρ e₀))
    (where (gcc₁ ... dec₁ ...) (t ρ e₁))
    (where (gcc₂ ... dec₂ ...) (t ρ e₂))
@@ -82,11 +86,39 @@
   [(t* ρ e ...) (gcc ... ... dec ... ...)
    (where ((gcc ... dec ...) ...) ((t ρ e) ...))])
 
+;; convert symbolic program to absolute program
+(define-metafunction L
+  ↓ : GCC -> (gcc ...)
+  [(↓ (gcc ... (: ℓ gcc_i ...) ...))
+   ((↓ᵢ symtab gcc_flattened) ...)
+   (where (gcc_flattened ...) (gcc ... gcc_i ... ...))
+   (where n_0 ,(length (term (gcc ...))))
+   (where (n_i ...) ,(map length (term ((gcc_i ...) ...))))
+   (where symtab ,(let*-values ([(_ ns)
+                                 (for/fold ([n_acc (term n_0)]
+                                            [n⋯ (list (term n_0))])
+                                           ([li (term (n_i ...))])
+                                   (let ([ni (+ n_acc li)])
+                                     (values ni (cons ni n⋯))))])
+                    (for/hash ([ℓ (term (ℓ ...))] [i (reverse (rest ns))])
+                      (values ℓ i))))])
+
+;; convert command to use absolute address
+(define-metafunction L
+  symtab@ : symtab $n -> n
+  [(symtab@ _ n) n]
+  [(symtab@ symtab ℓ) ,(hash-ref (term symtab) (term ℓ))])
+(define-metafunction L
+  ↓ᵢ : symtab gcc -> gcc
+  [(↓ᵢ symtab (any $n ...)) (any (symtab@ symtab $n) ...)]
+  [(↓ᵢ _ gcc) gcc])
+
 (define-metafunction L
   index-of : ρ x -> (n i)
   [(index-of ((y ...) ... (z ... x _ ...) _ ...) x)
    (,(length (term ((y ...) ...))) ,(length (term (z ...))))
-   (side-condition (not (member (term x) (term (y ... ...)))))])
+   (side-condition (not (member (term x) (term (y ... ...)))))]
+  [(index-of ρ x) ,(error (format "variable ~a not found in context ~a" (term x) (term ρ)))])
 
 (define-metafunction L
   fm : GCC -> string
@@ -100,9 +132,15 @@
      "\n"
      #:after-last "\n")])
 
+(define (dump-sym e) (printf (term (fm (T ,e)))))
+(define (dump e) (printf (term (fm (↓ (T ,e))))))
+
 ;; Example programs
 (define-term ex1
   ((λ (x) (ADD x x)) 21))
 
 (define-term ex2
   ((λ (x) (if x 2 3)) 42))
+
+(define-term ex3
+  (LET ([x 1] [y 2]) (MUL x y)))
